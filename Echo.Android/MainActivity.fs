80de4ab4df04f2784d4c5a81da0f3ae4b3bbf17e
valentemesmo <valentemesmo@gmail.com>
2019-11-06 07:33:16 -0300

    First commit

---
+++ Echo.Android/MainActivity.fs
@@ -0,0 +1,34 @@
+﻿namespace Echo.Android
+
+open System
+
+open Android.App
+open Android.Content
+open Android.OS
+open Android.Runtime
+open Android.Views
+open Android.Widget
+open MGNamespace
+open Android.Content.PM
+
+//type Resources = Echo.Android.Resource
+
+[<Activity (
+    Label = "Echo.Android"
+    , MainLauncher = true
+    , Icon = "@mipmap/icon"
+    , LaunchMode =  LaunchMode.SingleInstance
+)>]
+type MainActivity () =
+    inherit Microsoft.Xna.Framework.AndroidGameActivity ()
+
+    let mutable count:int = 1
+
+    override this.OnCreate (bundle) =
+
+        base.OnCreate (bundle)
+        let game = new Game1()
+        game.Run();
+
+        let view = game.Services.GetService<View>()
+        this.SetContentView(view);
---
+++ Echo.Desktop/Program.fs
@@ -0,0 +1,12 @@
+namespace MGNamespace
+
+module Program =
+
+    open System
+    open Microsoft.Xna.Framework
+
+    [<EntryPoint>]
+    let main argv =
+        use game = new Game1()
+        game.Run()
+        0
