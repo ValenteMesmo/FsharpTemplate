eaa68811f63f47e0ea95998919a7a6b0b576d3af
valentemesmo <valentemesmo@gmail.com>
2019-11-28 06:37:54 -0300

    popping balloons with mouse clicks

--- Echo.Common/TouchController.fs
+++ Echo.Common/TouchController.fs
@@ -3,6 +3,7 @@
     open Microsoft.Xna.Framework.Input.Touch
     open System.Collections.Generic
     open Microsoft.Xna.Framework
+    open Microsoft.Xna.Framework.Input
 
     type TouchController(Camera: Camera) =
         let touches = List<Vector2>()
@@ -12,6 +13,11 @@
             let touchCollection = TouchPanel.GetState()
             for touch in touchCollection do
                 touches.Add(Camera.GetWorldPosition(touch.Position))
+            
+            let mouse = Mouse.GetState()
+            if mouse.LeftButton = ButtonState.Pressed then
+                touches.Add(Camera.GetWorldPosition(mouse.Position.ToVector2()))
+
             ()
 
         member this.GetTouches() : List<Vector2> =
