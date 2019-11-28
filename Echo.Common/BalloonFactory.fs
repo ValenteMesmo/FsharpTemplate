80f42582609c342d68f3d8f489cae15d4e5bda15
valentemesmo <valentemesmo@gmail.com>
2019-11-27 23:01:36 -0300

    fixing balloon start position

--- Echo.Common/BalloonFactory.fs
+++ Echo.Common/BalloonFactory.fs
@@ -4,14 +4,14 @@ module BalloonFactory =
     open Balloon
 
     let Create(addToWorld) =
-        let mutable counter = 0
+        let mutable counter = 99
         let mutable change = 0
         let update obj =
             counter <- counter + 1
             if counter > 100 then
                 counter <- 0
                 let ballon = Create()
-                ballon.Y <- 500
+                ballon.Y <- 700
                 ballon.X <- change * GameConstants.BalloonSize
                 addToWorld ballon
 
